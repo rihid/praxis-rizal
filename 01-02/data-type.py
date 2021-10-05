e47f69e21a425b8e38e3d5059c530c1b9c2e1cbb
Rizal Hidayat <rizalhidayat006@gmail.com>
2021-10-05 12:50:57 +0700

    Note: Mengerjakan task di hari kedua | Python Data Type

---
+++ 01-02/data-type.py
@@ -0,0 +1,37 @@
+# Eksplorasi mengenai type data pada Python
+
+# String
+a = 'Hallo'
+print(type(a))
+print(a)
+
+# Integer & Float
+b = 3
+c = 1.5
+hasil = b/c
+print(type(b))
+print(type(c))
+print(hasil)
+
+# Boolean
+d = True
+print(type(d))
+
+# Tipe data yang datanya berkelompok
+# list
+dataHewan = ['Singa', 'Kuda', 'Jerapah']
+print(dataHewan)
+print(type(dataHewan))
+
+# Tuple
+dataShinobi = ('Naruto', 'Sasuke', 'Sakura')
+print(dataShinobi)
+print(type(dataShinobi))
+
+# Dict
+
+dataDiri = {'Nama' : 'Rizal', 'Alamat' : 'Ponorogo', 'Gender' : 'Laki-Laki'}
+print(dataDiri)
+print(type(dataDiri))
+# Tampilkan satu index
+print(dataDiri['Nama'])
