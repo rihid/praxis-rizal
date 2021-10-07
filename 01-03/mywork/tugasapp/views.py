b1be94cd22835dd4b5661e3aa4dd099d91b934fd
Rizal Hidayat <rizalhidayat006@gmail.com>
2021-10-07 20:11:11 +0700

    Note: Pekan 1 Pertemuan 4 | Meneruskan pertemuan sebelumnya mulai berinteraksi dengan database dan mulai CRUD--Folder masih sama seperti pertemuan sebelumnya

--- 01-03/mywork/tugasapp/views.py
+++ 01-03/mywork/tugasapp/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from . import models
+
 
 def index(request):
-    return HttpResponse(request, 'index.html')
+    if request.POST:
+
+        data = request.POST['fname']
+        print(data)
+        # Input data ke database
+        models.tugas.objects.create(nama = data)
+    # Nampilin data
+    hasil = models.tugas.objects.all()
+    
+    return render(request, 'index.html', {'isi': hasil})
+    # return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("Halaman About")
